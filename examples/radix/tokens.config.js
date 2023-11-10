64464f2eb95fcc9a297e4c72428b46117bc053e3
Drew Powers <1369770+drwpow@users.noreply.github.com>
2023-11-10 13:09:18 -0700

    Add Radix UI colors (#154)

---
+++ examples/radix/tokens.config.js
@@ -0,0 +1,8 @@
+import pluginCSS from '@cobalt-ui/plugin-css';
+import pluginSass from '@cobalt-ui/plugin-sass';
+import pluginJS from '@cobalt-ui/plugin-js';
+
+/** @type import('@cobalt-ui/core').Config */
+export default {
+  plugins: [pluginCSS(), pluginSass(), pluginJS()],
+};
