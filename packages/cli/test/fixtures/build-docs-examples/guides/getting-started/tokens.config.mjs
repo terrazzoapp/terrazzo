2ffe5429c5c5ccb2add6668787e5651a766b07f5
autarc <autarc@gmail.com>
2023-12-19 18:20:53 +0100

    Extend build test with docs examples

---
+++ packages/cli/test/fixtures/build-docs-examples/guides/getting-started/tokens.config.mjs
@@ -0,0 +1,9 @@
+import pluginCSS from '../../../../../../plugin-css/dist/index.js';
+import pluginJS from '../../../../../../plugin-js/dist/index.js';
+
+/** @type import('../../../../../../core').Config */
+export default {
+  tokens: './tokens.json',
+  outDir: './tokens/',
+  plugins: [pluginCSS(/* options */), pluginJS(/* options */)],
+};
