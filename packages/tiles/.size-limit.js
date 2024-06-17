e41361129b78b377d772bb8f62d3224f228f2f20
Drew Powers <drew@pow.rs>
2024-06-16 23:50:27 -0600

    Combine Terrazzo docs and Cobalt

--- packages/tiles/.size-limit.js
+++ packages/tiles/.size-limit.js
@@ -1,10 +1,10 @@
 export default [
-  { path: './dist/Button/Button.js', limit: '1 kB' },
-  { path: './dist/ButtonLink/ButtonLink.js', limit: '1 kB' },
-  { path: './dist/Fieldset/Fieldset.js', limit: '1 kB' },
-  { path: './dist/Select/Select.js', limit: '32 kB' },
-  { path: './dist/SubtleInput/SubtleInput.js', limit: '1 kB' },
-  { path: './dist/Switch/Switch.js', limit: '1 kB' },
-  { path: './dist/TokenType/TokenType.js', limit: '10 kB' },
-  { path: './dist/Tooltip/Tooltip.js', limit: '15 kB' },
+  { path: './dist/Button/Button.jsx', limit: '1 kB' },
+  { path: './dist/ButtonLink/ButtonLink.jsx', limit: '1 kB' },
+  { path: './dist/Fieldset/Fieldset.jsx', limit: '1 kB' },
+  { path: './dist/Select/Select.jsx', limit: '32 kB' },
+  { path: './dist/SubtleInput/SubtleInput.jsx', limit: '1 kB' },
+  { path: './dist/Switch/Switch.jsx', limit: '1 kB' },
+  { path: './dist/TokenType/TokenType.jsx', limit: '10 kB' },
+  { path: './dist/Tooltip/Tooltip.jsx', limit: '15 kB' },
 ];
