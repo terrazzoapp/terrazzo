b155f0c77dad0b9fac4148324925e005cb8871b1
Drew Powers <drew@pow.rs>
2024-06-30 13:42:24 -0700

    Update colorpicker docs (#263)

--- www/src/scripts/toc.js
+++ www/src/scripts/toc.js
@@ -18,6 +18,10 @@ function debounce(cb, timeout) {
 function calculateYs() {
   ys = [];
   for (const el of content.querySelectorAll('h2,h3,h4')) {
+    // skip over ToC heading
+    if (el.parentElement.classList.contains('toc')) {
+      continue;
+    }
     ys.push(window.scrollY + el.getBoundingClientRect().y);
   }
 }
@@ -39,11 +43,10 @@ addEventListener('scroll', () => {
   let nextI = 0;
   if (window.scrollY >= 0.3 * window.innerHeight) {
     for (let i = 1; i < ys.length; i++) {
-      if (window.scrollY + THRESHOLD_Y * window.innerHeight > ys[i]) {
-        nextI = i;
-      } else {
+      if (window.scrollY + THRESHOLD_Y * window.innerHeight < ys[i]) {
         break;
       }
+      nextI = i;
     }
   }
   if (nextI !== currentI) {
