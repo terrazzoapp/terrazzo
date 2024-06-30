54c50844c470ca87f49f9c81d4cd8a266bb4e063
Drew Powers <drew@pow.rs>
2024-06-30 17:30:07 -0600

    Lint errors

--- stylelint.config.js
+++ stylelint.config.js
@@ -6,6 +6,7 @@ export default {
     'alpha-value-notation': 'number',
     'declaration-block-no-redundant-longhand-properties': null, // this is fine
     'no-descending-specificity': null, // this often leads to unsolvable riddles
+    'number-max-precision': null, // in the running for dumbest rule ever created
     'order/properties-alphabetical-order': true, // let me find things
     'property-no-vendor-prefix': null, // we still need this
     'selector-class-pattern': null,
