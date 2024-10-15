a03490ae7181749f6e023051008211b897907805
Drew Powers <drew@pow.rs>
2024-10-14 21:41:34 -0600

    Docs: Minor tweak (#316)
    * Minor docs tweak
    
    * Chore: SB config update

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -1,15 +1,27 @@
+import path from 'node:path';
+
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-essentials'],
+  addons: [getAbsolutePath('@storybook/addon-essentials')],
+
   framework: {
-    name: '@storybook/react-vite',
+    name: getAbsolutePath('@storybook/react-vite'),
     options: {},
     disableTelemetry: true,
   },
+
   docs: {
     autodocs: false,
     docsMode: false,
   },
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+  },
 };
 export default config;
+
+function getAbsolutePath(value) {
+  return path.dirname(require.resolve(path.join(value, 'package.json')));
+}
