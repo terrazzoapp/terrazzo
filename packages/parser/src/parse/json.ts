078be5838b78fda77a5b188694d633bbbe38f31c
Drew Powers <drew@pow.rs>
2025-01-12 22:53:21 -0700

    Cleanup parser code (#406)

--- packages/parser/src/parse/json.ts
+++ packages/parser/src/parse/json.ts
@@ -1,4 +1,15 @@
-import type { AnyNode, MemberNode, ObjectNode, ValueNode } from '@humanwhocodes/momoa';
+import {
+  type AnyNode,
+  type DocumentNode,
+  type MemberNode,
+  type ObjectNode,
+  type ValueNode,
+  parse as parseJSON,
+  print,
+} from '@humanwhocodes/momoa';
+import type yamlToMomoa from 'yaml-to-momoa';
+import type Logger from '../logger.js';
+import type { InputSource } from '../types.js';
 
 export interface Visitor {
   enter?: (node: AnyNode, parent: AnyNode | undefined, path: string[]) => void;
@@ -25,17 +36,20 @@ export function isNode(value: unknown): boolean {
   return !!value && typeof value === 'object' && 'type' in value && typeof value.type === 'string';
 }
 
+export type ValueNodeWithIndex = ValueNode & { index: number };
+
 /** Get ObjectNode members as object */
-export function getObjMembers(node: ObjectNode): Record<string | number, ValueNode> {
-  const members: Record<string | number, ValueNode> = {};
+export function getObjMembers(node: ObjectNode): Record<string | number, ValueNodeWithIndex> {
+  const members: Record<string | number, ValueNodeWithIndex> = {};
   if (node.type !== 'Object') {
     return members;
   }
-  for (const m of node.members) {
+  for (let i = 0; i < node.members.length; i++) {
+    const m = node.members[i]!;
     if (m.name.type !== 'String') {
       continue;
     }
-    members[m.name.value] = m.value;
+    members[m.name.value] = { ...m.value, index: i };
   }
   return members;
 }
@@ -56,7 +70,8 @@ export function injectObjMembers(node: ObjectNode, members: MemberNode[] = []): 
 }
 
 /**
- * Variation of Momoa’s traverse(), which keeps track of global path
+ * Variation of Momoa’s traverse(), which keeps track of global path.
+ * Allows mutation of AST (along with any consequences)
  */
 export function traverse(root: AnyNode, visitor: Visitor) {
   /**
@@ -101,6 +116,92 @@ export function traverse(root: AnyNode, visitor: Visitor) {
 }
 
 /** Determine if an input is likely a JSON string */
-export function maybeJSONString(input: string): boolean {
+export function maybeRawJSON(input: string): boolean {
   return typeof input === 'string' && input.trim().startsWith('{');
+}
+
+/** Find Momoa node by traversing paths */
+export function findNode<T = AnyNode>(node: AnyNode, path: string[]): T | undefined {
+  if (!path.length) {
+    return;
+  }
+
+  let nextNode: AnyNode | undefined;
+
+  switch (node.type) {
+    // for Document nodes, dive into body for “free” (not part of the path)
+    case 'Document': {
+      return findNode(node.body, path);
+    }
+    case 'Object': {
+      const [member, ...rest] = path;
+      nextNode = node.members.find((m) => m.name.type === 'String' && m.name.value === member)?.value;
+      if (nextNode && rest.length) {
+        return findNode(nextNode, path.slice(1));
+      }
+      break;
+    }
+    case 'Array': {
+      const [_index, ...rest] = path;
+      const index = Number.parseInt(_index!, 10);
+      nextNode = node.elements[index]?.value;
+      if (nextNode && rest.length) {
+        return findNode(nextNode, path.slice(1));
+      }
+      break;
+    }
+  }
+
+  return nextNode as T;
+}
+
+export interface ToMomoaOptions {
+  filename?: URL;
+  continueOnError?: boolean;
+  logger: Logger;
+  yamlToMomoa?: typeof yamlToMomoa;
+}
+
+export function toMomoa(
+  input: string | Record<string, any>,
+  { continueOnError, filename, logger, yamlToMomoa }: ToMomoaOptions,
+): InputSource {
+  let src = '';
+  if (typeof input === 'string') {
+    src = input;
+  }
+  let document = {} as DocumentNode;
+  if (typeof input === 'string' && !maybeRawJSON(input)) {
+    if (yamlToMomoa) {
+      try {
+        document = yamlToMomoa(input); // if string, but not JSON, attempt YAML
+      } catch (err) {
+        logger.error({ message: String(err), filename, src: input, continueOnError });
+      }
+    } else {
+      logger.error({
+        group: 'parser',
+        message: `Install \`yaml-to-momoa\` package to parse YAML, and pass in as option, e.g.:
+
+    import { parse } from '@terrazzo/parser';
+    import yamlToMomoa from 'yaml-to-momoa';
+
+    parse(yamlString, { yamlToMomoa });`,
+        continueOnError: false, // fail here; no point in continuing
+      });
+    }
+  } else {
+    document = parseJSON(
+      typeof input === 'string' ? input : JSON.stringify(input, undefined, 2), // everything else: assert it’s JSON-serializable
+      {
+        mode: 'jsonc',
+        ranges: true,
+        tokens: true,
+      },
+    );
+  }
+  if (!src) {
+    src = print(document, { indent: 2 });
+  }
+  return { src, document };
 }
