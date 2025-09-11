65ab26312f55ced5b30f4d8830258abd7a56b60e
Drew Powers <drew@pow.rs>
2025-09-10 20:03:10 -0600

    ci: fix Node 24 tests

--- packages/token-tools/test/css.test.ts
+++ packages/token-tools/test/css.test.ts
@@ -177,7 +177,7 @@ describe('transformBoolean', () => {
 });
 
 describe('transformColor', () => {
-  const tests: Test<Parameters<typeof transformColor>, ReturnType<typeof transformColor>>[] = [
+  const tests: Test<Parameters<typeof transformColor>, string | RegExp | Record<string, string | RegExp>>[] = [
     [
       'string',
       {
@@ -363,10 +363,10 @@ describe('transformColor', () => {
         ],
         want: {
           success: {
-            '.': 'oklch(0.9 0.1 40)',
-            srgb: 'oklch(0.8871159807250525 0.06383317651475744 44.99492562496044)',
-            p3: 'oklch(0.887964813585299 0.08088218405569154 44.274026638671025)',
-            rec2020: 'oklch(0.9000000000000001 0.10000000000000017 39.99999999999983)',
+            '.': /oklch\(0.9 0.1 40\)/,
+            srgb: /oklch\(0\.88711\d* 0\.06383\d* 44\.99492\d*\)/,
+            p3: /oklch\(0\.88796\d* 0\.08088\d* 44\.27402\d*\)/,
+            rec2020: /oklch\(0\.9\d* 0\.(09|1)\d* (39|40)\.?\d*\)/,
           },
         },
       },
@@ -422,24 +422,20 @@ describe('transformColor', () => {
       },
     ],
   ];
-  it.each(tests)('%s', (testName, { given, want }) => {
+  it.each(tests)('%s', (_, { given, want }) => {
     let result: typeof want.success;
-
     try {
       result = transformColor(...given);
+      if (typeof result === 'string') {
+        expect(result).toMatch(want.success as string);
+      } else {
+        for (const k of Object.keys(result as any)) {
+          expect((result as any)[k]).toMatch((want.success as any)[k]);
+        }
+      }
     } catch (err) {
       expect((err as Error).message).toBe(want.error);
     }
-
-    // TODO: node 24 rounding error
-    if (testName === 'oklch (out of gamut)' && result && typeof result === 'object') {
-      expect(result['.']).toBe('oklch(0.9 0.1 40)');
-      expect(result.p3).toMatch(/oklch\(0\.88796\d* 0\.08088\d* 44\.27402\d*\)/);
-      expect(result.rec2020).toMatch(/oklch\(0\.9\d* ((0\.09\d*)|(0\.1\d*)) (39|40)\.?\d*\)/);
-      expect(result.srgb).toMatch(/oklch\(0\.8871\d* 0\.06383\d* 44\.99492\d*\)/);
-    }
-
-    expect(result).toEqual(want.success);
   });
 });
 
