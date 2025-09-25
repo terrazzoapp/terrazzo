34f491e0df5a02aaa01be7c26833a4c97568116a
David Leger <david.leger@getkard.com>
2025-09-25 15:48:41 -0300

    feat: add fontFamily input

---
+++ packages/token-lab/src/components/EditableFontFamilyToken/EditableFontFamilyToken.module.d.css.ts
@@ -0,0 +1,8 @@
+declare const classNames: {
+  readonly container: 'container';
+  readonly item: 'item';
+  readonly list: 'list';
+  readonly removeButton: 'removeButton';
+  readonly input: 'input';
+};
+export default classNames;
