caeec77effaa2870666aac153536e90293853c70
Drew Powers <drew@pow.rs>
2025-10-06 23:41:10 -0600

    ci: fix changesets typo, allow Tailwind test more time (#563)

--- packages/plugin-tailwind/vitest.config.ts
+++ packages/plugin-tailwind/vitest.config.ts
@@ -1,8 +1,7 @@
-import os from 'node:os';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    testTimeout: os.platform() === 'win32' ? 10000 : 5000,
+    testTimeout: 10_000,
   },
 });
