c2fbea7fa6a1f6d0291b7c51e8deea295a301260
Drew Powers <drew@pow.rs>
2025-10-24 19:56:16 -0600

    Fix aria-current

--- www/src/lib/navigation.ts
+++ www/src/lib/navigation.ts
@@ -1,6 +1,6 @@
 export function getAriaCurrent(testPathname: string, currentPathname: string | URL): 'page' | 'location' | undefined {
   const currentLocation = new URL(currentPathname);
-  if (currentLocation.pathname === testPathname) {
+  if (currentLocation.pathname.replace(/\/?$/, '/') === testPathname.replace(/\/?$/, '/')) {
     return 'page';
   }
   if (testPathname !== '/' && currentLocation.pathname.startsWith(testPathname)) {
