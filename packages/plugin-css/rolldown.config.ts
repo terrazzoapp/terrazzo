1f80c80290dc041081deb2139973d48c633ed71f
Bill Collins <bill@bill.works>
2025-11-22 13:40:29 +0000

    Fix bundling woes for plugin-css by way of example

--- packages/plugin-css/rolldown.config.ts
+++ packages/plugin-css/rolldown.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'rolldown';
 import { dts } from 'rolldown-plugin-dts';
 
+import pkg from './package.json';
+
 export default defineConfig({
   input: {
     index: './src/index.ts',
   },
   platform: 'browser',
-  plugins: [dts()],
+  plugins: [dts({resolve: true})],
+  external: Object.keys(pkg.dependencies).flatMap(k => [k, new RegExp(`^${k}\/.*`)]),
   output: {
     dir: 'dist',
     format: 'es',
