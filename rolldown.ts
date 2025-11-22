a8c1e925bfe4c220be39ba82add5dd0c98c6fec2
Bill Collins <bill@bill.works>
2025-11-22 22:26:45 +0000

    Merge configs instead of blatting

--- rolldown.ts
+++ rolldown.ts
@@ -2,20 +2,21 @@ import {type RolldownOptions} from 'rolldown';
 import {dts} from 'rolldown-plugin-dts';
 import {readFileSync} from 'node:fs';
 import {join} from "node:path";
-
+import {merge} from 'merge-anything';
 
 export const defineConfig = (options: RolldownOptions): RolldownOptions => {
-
     const pkg = JSON.parse(readFileSync(join(process.cwd(), 'package.json'), 'utf8'))
-    return {
-        platform: 'browser',
-        plugins: [dts({resolve: true})],
-        external: Object.keys(pkg.dependencies ?? {}).flatMap(k => [k, new RegExp(`^${k}/`)]),
-        output: {
-            dir: 'dist',
-            format: 'es',
-            sourcemap: true,
+    return merge(
+        {
+            platform: 'browser',
+            plugins: [dts({resolve: true})],
+            external: Object.keys(pkg.dependencies ?? {}).flatMap(k => [k, new RegExp(`^${k}/`)]),
+            output: {
+                dir: 'dist',
+                format: 'es',
+                sourcemap: true,
+            },
         },
-        ...options,
-    }
+        options
+    )
 }
