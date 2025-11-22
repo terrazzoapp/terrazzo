1b0d14de0858eacae5505b2a246569302db51a45
Bill Collins <bill.collins@hp.com>
2025-11-22 15:02:16 +0000

    Fix rolldown config

--- packages/json-schema-tools/rolldown.config.ts
+++ packages/json-schema-tools/rolldown.config.ts
@@ -1,16 +1,7 @@
-import { defineConfig } from 'rolldown';
-import { dts } from 'rolldown-plugin-dts';
+import { defineConfig } from '../../rolldown';
 
 export default defineConfig({
   input: {
     index: './src/index.ts',
-  },
-  platform: 'browser',
-  plugins: [dts()],
-  external: ['@humanwhocodes/momoa'],
-  output: {
-    dir: 'dist',
-    format: 'es',
-    sourcemap: true,
   },
 });
