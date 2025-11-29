ad2da0500482fa14a992e6b4e8653b9733742b00
Bill Collins <bill@bill.works>
2025-11-29 09:31:54 +0000

    Add yaml-to-momoa as optional peer dep where referenced

--- rolldown.ts
+++ rolldown.ts
@@ -10,11 +10,10 @@ export const defineConfig = (options: RolldownOptions): RolldownOptions => {
     {
       platform: 'browser',
       plugins: [dts({ resolve: true })],
-      external: [
-        'yaml-to-momoa',
-        ...Object.keys(pkg.peerDependencies ?? {}),
-        ...Object.keys(pkg.dependencies ?? {}),
-      ].flatMap((k) => [k, new RegExp(`^${k}/`)]),
+      external: [...Object.keys(pkg.peerDependencies ?? {}), ...Object.keys(pkg.dependencies ?? {})].flatMap((k) => [
+        k,
+        new RegExp(`^${k}/`),
+      ]),
       output: {
         dir: 'dist',
         format: 'es',
