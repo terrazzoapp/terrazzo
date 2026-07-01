f694aa25ff2636e4d0c21c141d51b1756c47e767
Bill Collins <bill@bill.works>
2026-07-01 12:50:28 +0100

    poc use tsc (#782)
    * Add .npmignore to token-types
    
    * e.g. use tsc

--- packages/token-tools/rolldown.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '../../rolldown';
-
-export default defineConfig({
-  input: {
-    index: './src/index.ts',
-    css: './src/css/index.ts',
-    js: './src/js/index.ts',
-  },
-});
