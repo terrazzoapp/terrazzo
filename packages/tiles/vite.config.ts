262ebccfbe895b93b7123b5a94f4073bc03f5ff6
Drew Powers <drew@pow.rs>
2026-07-26 10:36:00 -0600

    Consolidate on ts7, remove older bundling setups

--- packages/tiles/vite.config.ts
+++ packages/tiles/vite.config.ts
@@ -1,9 +1,24 @@
+import fs from 'node:fs';
+
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 import { defineConfig } from 'vitest/config';
+
+const PKG_JSON = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 
 /** @see https://vitejs.dev/config/ */
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: '@terrazzo/tiles',
+      fileName: 'index',
+    },
+    rolldownOptions: {
+      external: [...Object.keys(PKG_JSON.dependencies), ...Object.keys(PKG_JSON.devDependencies)],
+    },
+  },
   test: {
     environment: 'jsdom',
     restoreMocks: true,
